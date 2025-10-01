0374daf4d1ff427d789d891f27ed3e4b78db2c6f
SepyGit <shareckgratonnathan@gmail.com>
2025-10-01 19:02:43 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:'center', marginTop:'50px'}}>
+      <h1>🚀 Welcome to SocialNet</h1>
+      <p>Next.js project ready for Vercel deployment with a working pages folder.</p>
+    </div>
+  );
+}
